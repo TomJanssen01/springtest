90c80d703f69df6b07a088360d973759e035520b
Tom Janssen <tom.janssen@student.thomasmore.be>
2020-10-09 13:49:37 +0200

    initial files

---
+++ src/main/java/thomasmore/be/springtest/SpringtestApplication.java
@@ -0,0 +1,13 @@
+package thomasmore.be.springtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringtestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringtestApplication.class, args);
+    }
+
+}
