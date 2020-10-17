055acd4d7351b0d40f76169ebad5121d3275d981
Tom Janssen <tom.janssen@student.thomasmore.be>
2020-10-17 11:51:24 +0200

    add dev.properties & prod.properties3

--- src/main/java/thomasmore/be/springtest/HomeController.java
+++ src/main/java/thomasmore/be/springtest/HomeController.java
@@ -1,18 +1,26 @@
 package thomasmore.be.springtest;
 
+import lombok.Value;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.logging.Logger;
 
 
 @Controller
+@RequestMapping("/admin")
 public class HomeController {
-
+//    private Logger logger = LoggerFactory.getLogger(HomeController.class);
     final String applicationName = "Mijn applicatie om Spring te testen";
+
 
     @GetMapping("/")
     public String home(Model model) {
         model.addAttribute("appName", applicationName);
         return "home";
     }
+
 }
